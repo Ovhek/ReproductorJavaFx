72042209b6ac72480bc42d1ce796d37749deef95
=Alexandru Cruceat <=cruceat.alexnadru.ioan@alumnat.copernic.cat>
2022-11-02 12:29:33 +0100

    Added App Layouts Separation

---
+++ m03uf5pracma/src/main/java/acisum/m03uf5pracma/Layout_leftController.java
@@ -0,0 +1,27 @@
+package acisum.m03uf5pracma;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Cole
+ */
+public class Layout_leftController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
