90f82dba7f00bb479977f7c77837e3020fb56ed4
=Alexandru Cruceat <=cruceat.alexnadru.ioan@alumnat.copernic.cat>
2022-11-02 11:42:04 +0100

    Merge del manel

---
+++ m03uf5pracma/src/main/java/acisum/m03uf5pracma/PrimaryController.java
@@ -0,0 +1,12 @@
+package acisum.m03uf5pracma;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ m03uf5pracma/src/main/java/acisum/m03uf5pracma/SecondaryController.java
@@ -0,0 +1,12 @@
+package acisum.m03uf5pracma;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
