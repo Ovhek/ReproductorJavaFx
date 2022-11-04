dcff0438ab869627bb184e2d9efadc3fc9310a56
=Alexandru Cruceat <=cruceat.alexnadru.ioan@alumnat.copernic.cat>
2022-11-04 13:12:25 +0100

    UI del Reproductor del MP3

--- m03uf5pracma/src/main/java/acisum/m03uf5pracma/LayoutBottomController.java
+++ m03uf5pracma/src/main/java/acisum/m03uf5pracma/LayoutBottomController.java
@@ -1,27 +1,130 @@
 package acisum.m03uf5pracma;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
-
+import acisum.m03uf5pracma.Utils.Utils;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaException;
+import javafx.scene.media.MediaPlayer;
 
-/**
- * FXML Controller class
- *
- * @author Cole
- */
+
 public class LayoutBottomController extends Controller implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Button playerBackward;
+
+    @FXML
+    private Button playerForward;
+
+    @FXML
+    private Button playerLoop;
+
+    @FXML
+    private Button playerMuteUnMute;
+
+    @FXML
+    private Button playerPlayPause;
+
+    @FXML
+    private Button playerShuffleMode;
+
+    @FXML
+    private Slider playerSlider;
+
+    @FXML
+    private Button playerStop;
+
+    @FXML
+    private Label playerTime;
+
+    @FXML
+    private Slider playerVolume;
+
+    MediaPlayer mediaPlayer = null;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        
+        
+        
+        try {
+            String path = Utils.getMp3Path(this, "A");
+            mediaPlayer = Utils.getMediaPlayer(path);
+        } catch (MediaException e) {
+            System.out.println("AAAA");
+        }
+        
+        
+    }
+    
+    @FXML
+    void actionPlayerBackward(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionPlayerForward(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionPlayerMuteUnMute(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionPlayerPlayPause(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionPlayerShuffleMode(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionPlayerStop(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mouseDragDetectedPlayerSlider(MouseEvent event) {
+
+    }
+
+    @FXML
+    void mouseDragDetectedPlayerVolume(MouseEvent event) {
+
+    }
+
+    @FXML
+    void mouseDragDonePlayerSlider(DragEvent event) {
+
+    }
+
+    @FXML
+    void mouseDragDonePlayerVolume(DragEvent event) {
+
+    }
+
+    @FXML
+    void mousePressedPlayerSlider(MouseEvent event) {
+
+    }
+
+    @FXML
+    void mousePressedPlayerVolume(MouseEvent event) {
+
+    }
     
 }
+
+
---
+++ m03uf5pracma/src/main/java/acisum/m03uf5pracma/Utils/Utils.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package acisum.m03uf5pracma.Utils;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaException;
+import javafx.scene.media.MediaPlayer;
+
+/**
+ *
+ * @author Cole
+ */
+public abstract class Utils {
+    
+    
+    public static MediaPlayer getMediaPlayer(String mp3Path) throws MediaException
+    {
+        return new MediaPlayer(new Media(mp3Path));
+    }
+    
+    public static String getMp3Path(Object instance, String songName)
+    {
+        var test = instance.getClass().getClassLoader().getResource("sounds/");
+        
+        return instance.getClass().getClassLoader().getResource("sounds/"+songName+".mp3").toString();
+    }
+}
