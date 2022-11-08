265bbc5ee07471d7ee803772c04d2b9a673c1c2f
=Jose Antonio Bermudo Olea <=bermudo.olea.jose.antonio@alumnat.copernic.cat>
2022-11-08 13:55:48 +0100

    Merge origin/Develop into Jose
    Conflicts:
    	m03uf5pracma/src/main/java/model/Fmp3.java
    	m03uf5pracma/src/main/resources/acisum/m03uf5pracma/layoutBottom.fxml
    	m03uf5pracma/src/main/resources/acisum/m03uf5pracma/layoutCenter.fxml
    	m03uf5pracma/src/main/resources/acisum/m03uf5pracma/main.fxml

--- m03uf5pracma/src/main/java/acisum/m03uf5pracma/LayoutCenterController.java
+++ m03uf5pracma/src/main/java/acisum/m03uf5pracma/LayoutCenterController.java
@@ -4,17 +4,21 @@ package acisum.m03uf5pracma;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-
+import acisum.m03uf5pracma.Utils.Utils;
 import java.net.URL;
+import java.nio.file.Path;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import model.FileUtils;
 import model.Fmp3;
 import model.PlayList;
 
@@ -24,13 +28,14 @@ import model.PlayList;
  * @author Cole
  */
 public class LayoutCenterController extends Controller implements Initializable {
+
     PlayList playList;
     ObservableList<Fmp3> elements = FXCollections.observableArrayList();
     ObservableList<Fmp3> filtroListas = FXCollections.observableArrayList();
-    
+
     @FXML
     public Label labListTitle;
-    @FXML 
+    @FXML
     public ListView<Fmp3> lvMP3;
     @FXML
     public TextField textSearh;
@@ -40,13 +45,46 @@ public class LayoutCenterController extends Controller implements Initializable 
     public Button btnDeleteMP3;
     @FXML
     public Button btnAddMP3;
-    
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       lvMP3.setItems(elements);
+        lvMP3.setItems(elements);
+    }
+
+    @FXML
+    public void onActionBtnAdd(ActionEvent event) {
+
+        if (playList != null) {
+            Path path = FileUtils.getMP3Fromfile();
+            
+                String root = Utils.normalizeURLFormat(path.toString());
+                String fileName = Utils.getFileName(root);
+                Fmp3 fmp3 = new Fmp3(fileName, "", "", root);
+
+                elements.add(fmp3);
+                lvMP3.refresh();
+            
+        }
+        
+
+    }
+
+    public void setPlayList(PlayList playList) {
+        this.playList = playList;
+        labListTitle.setText(this.playList.getNombreLista());
+        elements = FXCollections.observableArrayList(this.playList.getPlayList());
+        lvMP3.setItems(elements);
+    }
+
+    public void borrarLista() {
+
+        labListTitle.setText("List Title");
+        this.playList = null;
+        elements.clear();
+        lvMP3.setItems(elements);
     }
 
     public PlayList getPlayList() {
@@ -84,21 +122,5 @@ public class LayoutCenterController extends Controller implements Initializable 
     public Button getBtnAddMP3() {
         return btnAddMP3;
     }
-
-    public void setPlayList(PlayList playList) {
-        this.playList = playList;
-        labListTitle.setText(this.playList.getNombreLista());
-        elements = FXCollections.observableArrayList(this.playList.getPlayList());
-        lvMP3.setItems(elements);
-    }
-    
-    public void borrarLista(){
-        
-        labListTitle.setText("List Title");
-        this.playList = null;
-        elements.clear();
-        lvMP3.setItems(elements);
-    }
-
 
 }
