0350b7640bf9c813b9b413948d0d24f5d30692a0
=Jose Antonio Bermudo Olea <=bermudo.olea.jose.antonio@alumnat.copernic.cat>
2022-11-08 12:35:47 +0100

    Merge origin/Develop into Jose
    Conflicts:
    	m03uf5pracma/src/main/java/model/Fmp3.java
    	m03uf5pracma/src/main/resources/acisum/m03uf5pracma/layoutBottom.fxml
    	m03uf5pracma/src/main/resources/acisum/m03uf5pracma/layoutCenter.fxml
    	m03uf5pracma/src/main/resources/acisum/m03uf5pracma/main.fxml

--- m03uf5pracma/src/main/java/acisum/m03uf5pracma/LayoutCenterController.java
+++ m03uf5pracma/src/main/java/acisum/m03uf5pracma/LayoutCenterController.java
@@ -7,9 +7,16 @@ package acisum.m03uf5pracma;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import model.Fmp3;
+import model.PlayList;
 
 /**
  * FXML Controller class
@@ -17,16 +24,81 @@ import javafx.scene.control.Label;
  * @author Cole
  */
 public class LayoutCenterController extends Controller implements Initializable {
-
+    PlayList playList;
+    ObservableList<Fmp3> elements = FXCollections.observableArrayList();
+    ObservableList<Fmp3> filtroListas = FXCollections.observableArrayList();
     
     @FXML
     public Label labListTitle;
+    @FXML 
+    public ListView<Fmp3> lvMP3;
+    @FXML
+    public TextField textSearh;
+    @FXML
+    public Button btnSortMP3;
+    @FXML
+    public Button btnDeleteMP3;
+    @FXML
+    public Button btnAddMP3;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+       lvMP3.setItems(elements);
+    }
+
+    public PlayList getPlayList() {
+        return playList;
+    }
+
+    public ObservableList<Fmp3> getElements() {
+        return elements;
+    }
+
+    public ObservableList<Fmp3> getFiltroListas() {
+        return filtroListas;
+    }
+
+    public Label getLabListTitle() {
+        return labListTitle;
+    }
+
+    public ListView<Fmp3> getLvMP3() {
+        return lvMP3;
+    }
+
+    public TextField getTextSearh() {
+        return textSearh;
+    }
+
+    public Button getBtnSortMP3() {
+        return btnSortMP3;
+    }
+
+    public Button getBtnDeleteMP3() {
+        return btnDeleteMP3;
+    }
+
+    public Button getBtnAddMP3() {
+        return btnAddMP3;
+    }
+
+    public void setPlayList(PlayList playList) {
+        this.playList = playList;
+        labListTitle.setText(this.playList.getNombreLista());
+        elements = FXCollections.observableArrayList(this.playList.getPlayList());
+        lvMP3.setItems(elements);
+    }
     
+    public void borrarLista(){
+        
+        labListTitle.setText("List Title");
+        this.playList = null;
+        elements.clear();
+        lvMP3.setItems(elements);
+    }
+
+
 }
